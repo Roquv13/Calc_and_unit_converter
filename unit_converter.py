0954dfdd66c547b46507d3c5033309457c4f9c4f
Roquv13 <czarekolszewski99@gmail.com>
2023-10-18 17:20:25 +0200

    Add mass_unit_dic

--- unit_converter.py
+++ unit_converter.py
@@ -1,4 +1,8 @@
-def mass_units_SI(value, unit_in, unit_out):
+def mass_units_dic():
     SI = {'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000}
+    return SI
+
+def mass_units_SI(value, unit_in, unit_out):
+    SI = mass_units_dic
     result = value * SI[unit_in]/SI[unit_out]
-    return result
+    return result, SI
