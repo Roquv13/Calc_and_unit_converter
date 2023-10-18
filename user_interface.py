cdc160c05951c1c443cd463eba3468de4cf62340
Roquv13 <czarekolszewski99@gmail.com>
2023-10-18 20:28:13 +0200

    Simple unit converter with user input

--- user_interface.py
+++ user_interface.py
@@ -1,12 +1,24 @@
-import unit_converter
-import calculator
+SI_mass = {'g':0.001, 'dag':0.01, 'kg':1.0, 't':1000}
+SI_len = {'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000.}
 
-#Mass converter
+#Define convert function
+def convert(value, unit_in, unit_out):
+    if unit_in in SI_mass:
+        default_unit = SI_mass
+    elif unit_in in SI_len:
+        default_unit = SI_len
+    result = value * default_unit[unit_in]/default_unit[unit_out]
+    return result
+
+# Mass converter
 print("Mass unit converter")
-unit_available = unit_converter.mass_units_keys()
-print("It supports: ", unit_available)
+print("It supports:", "\nMass: ", list(SI_mass.keys()), "\nLenght: ", list(SI_len.keys()))
+
+# User input
 value = float(input("Enter value to convert: "))
 unit_in = input("Enter initial unit: ")
 unit_out = input("Enter unit to convert to: ")
-result = unit_converter.mass_convert(value, unit_in, unit_out)
-print(result)
+
+# Display result
+result = convert(value, unit_in, unit_out)
+print("Result: ", result, unit_out)
