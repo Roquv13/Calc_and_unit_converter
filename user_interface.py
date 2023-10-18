b861c90fe7b1619477f2a43d8eb6f9afc2124e59
Roquv13 <czarekolszewski99@gmail.com>
2023-10-18 18:30:52 +0200

    Adding basic functionality and users input

--- user_interface.py
+++ user_interface.py
@@ -1,2 +1,12 @@
 import unit_converter
 import calculator
+
+#Mass converter
+print("Mass unit converter")
+unit_available = unit_converter.mass_units_keys()
+print("It supports: ", unit_available)
+value = float(input("Enter value to convert: "))
+unit_in = input("Enter initial unit: ")
+unit_out = input("Enter unit to convert to: ")
+result = unit_converter.mass_convert(value, unit_in, unit_out)
+print(result)
