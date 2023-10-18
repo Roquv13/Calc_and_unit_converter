7dbbc0573ed667718cfd23d6b79133755539c084
Roquv13 <czarekolszewski99@gmail.com>
2023-10-18 17:35:36 +0200

    Add mass unit keys

--- unit_converter.py
+++ unit_converter.py
@@ -1,8 +1,12 @@
 def mass_units_dic():
-    SI = {'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000}
+    SI = {'g':0.001, 'dag':0.01, 'kg':1.0, 't':1000}
     return SI
 
-def mass_units_SI(value, unit_in, unit_out):
+def mass_units_keys():
+    SI_keys = mass_units_dic
+    return SI_keys
+
+def mass_convert(value, unit_in, unit_out):
     SI = mass_units_dic
     result = value * SI[unit_in]/SI[unit_out]
-    return result, SI
+    return result
