1db4118920deb0d18c799443a06cefc4c6727e0e
Roquv13 <czarekolszewski99@gmail.com>
2023-10-18 18:27:56 +0200

    Add mass dictionary, keys of dic and converting mass

--- unit_converter.py
+++ unit_converter.py
@@ -1,12 +1,13 @@
-def mass_units_dic():
-    SI = {'g':0.001, 'dag':0.01, 'kg':1.0, 't':1000}
-    return SI
+SI_mass = {'g':0.001, 'dag':0.01, 'kg':1.0, 't':1000}
+
+def mass_units_dic(SI_mass):
+    return SI_mass
 
 def mass_units_keys():
-    SI_keys = mass_units_dic
+    SI_keys = list(SI_mass.keys())
     return SI_keys
 
 def mass_convert(value, unit_in, unit_out):
-    SI = mass_units_dic
+    SI = {'g':0.001, 'dag':0.01, 'kg':1.0, 't':1000}
     result = value * SI[unit_in]/SI[unit_out]
     return result
