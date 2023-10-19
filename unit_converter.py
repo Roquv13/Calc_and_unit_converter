7f4808d0b5937d57e5d41e9b5511ae0111b4c0f2
Roquv13 <czarekolszewski99@gmail.com>
2023-10-19 23:14:12 +0200

    Add functions for unit converter and delete not used functions

--- unit_converter.py
+++ unit_converter.py
@@ -1,24 +1,29 @@
+# Units SI dicionaries
 SI_mass = {'g':0.001, 'dag':0.01, 'kg':1.0, 't':1000}
 SI_len = {'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000.}
 
-#Define convert function
+def get_value(prompt):
+    while True:
+        value_str = input(prompt)
+        if value_str.isdigit():
+            return float(value_str)
+        print("Invalid input. Input must be number.")
+
+
+def get_unit(prompt):
+    while True:
+        opertion_sign = input(prompt)
+        if opertion_sign in SI_mass or SI_len:
+            return opertion_sign
+        print("Invalid input. Input must be sign from these list: ", SI_mass, SI_len)
+
 def convert(value, unit_in, unit_out):
     if unit_in in SI_mass:
-        default_unit = SI_mass
+        result = value * SI_mass[unit_in]/SI_mass[unit_out]
     elif unit_in in SI_len:
-        default_unit = SI_len
-    result = value * default_unit[unit_in]/default_unit[unit_out]
+        result = value * SI_len[unit_in]/SI_len[unit_out]
     return result
 
-# Mass converter
-print("Mass unit converter")
-print("It supports:", "\nMass: ", list(SI_mass.keys()), "\nLenght: ", list(SI_len.keys()))
-
-# User input
-value = float(input("Enter value to convert: "))
-unit_in = input("Enter initial unit: ")
-unit_out = input("Enter unit to convert to: ")
-
-# Display result
-result = convert(value, unit_in, unit_out)
-print("Result: ", result, unit_out)
+def units_list():
+    print("Mass unit converter")
+    print("It supports:", "\nMass: ", list(SI_mass.keys()), "\nLenght: ", list(SI_len.keys()))
