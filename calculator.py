1372a752fc1394eb288c194c5cebc6f189d9ac5d
Roquv13 <czarekolszewski99@gmail.com>
2023-10-19 20:36:01 +0200

    Add get_number function

--- calculator.py
+++ calculator.py
@@ -1,3 +1,23 @@
-num1 = input("Enter first number for calculations: ")
-num2 = input("Enter second number for calculations: ")
+operations_list = ['+', '-', '*', '/']
+
+def get_number(prompt):
+    while True:
+        num_str = input(prompt)
+        if num_str.isdigit() or (num_str.startswith("-") and num_str[1:].isdigit()):
+            return float(num_str)
+        print("Invalid input. Input must be positive or negative number.")
+
+print("Calculator supports these operations", operations_list)
+
+num1 = get_number("Enter first number for calculations: ")
+num2 = get_number("Enter second number for calculations: ")
 operation = input("Enter operations to do: ")
+
+def calculations(first_number, second_number, operation_sign):
+    if operation == "+":
+        result = float(first_number) + float(second_number)
+        return result
+
+if operation in operations_list:
+    result = calculations(num1, num2, operation)
+    print(result)
