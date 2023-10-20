b37e3c8b793c72f398f23428daa27168a45090fe
Roquv13 <czarekolszewski99@gmail.com>
2023-10-20 17:54:40 +0200

    Add support for root calculations and add new function for getting result of calculations

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def operations_list():
-    operations_list = ['+', '-', '*', '/', '^', 'mod']
+    operations_list = ['+', '-', '*', '/', '^', 'root', 'mod']
     return operations_list
 
 def get_number(prompt):
@@ -35,15 +35,26 @@ def calculations(first_number, second_number, operation):
     if operation == "^":
         result = first_number ** second_number
         return result
+    if operation == "root":
+        result = first_number ** (1/second_number)
+        return result
     if operation == "mod":
         result = first_number % second_number
         return result
 
-def get_number(prompt):
-    while True:
-        num_str = input(prompt)
-        if num_str == "exit":
-            return num_str
-        if num_str.isdigit() or (num_str.startswith("-") and num_str[1:].isdigit()):
-            return float(num_str)
-        print("Invalid input. Please enter a number.")    
+def get_result():
+    operation = get_opertion("Enter operations to do: ")
+
+    if operation in operations_list():
+        if operation == "^":
+            num1 = get_number("Enter number to be exponentiated: ")
+            num2 = get_number("Enter degree of exponentiation: ")
+        elif operation == "root":
+            num1 = get_number("Enter number to calculate root")
+            num2 = get_number("Enter degree of root: ")
+        else:
+            num1 = get_number("Enter first number for calculations: ")
+            num2 = get_number("Enter second number for calculations: ")
+        
+        result = calculations(num1, num2, operation)
+        return print("Result of calculations: ", result)
