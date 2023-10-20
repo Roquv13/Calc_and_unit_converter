d2a9f931c5c1805b566b78657638a36ffdc575f6
Roquv13 <czarekolszewski99@gmail.com>
2023-10-20 19:10:27 +0200

    Add support for convert temperature units

--- unit_converter.py
+++ unit_converter.py
@@ -1,10 +1,11 @@
 # Units SI dicionaries
 SI_mass = {'g':0.001, 'dag':0.01, 'kg':1.0, 't':1000}
-SI_len = {'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000.}
+SI_len = {'mm':0.001, 'cm':0.01, 'm':1.0, 'km':1000}
+SI_temp = ['C', 'F', 'K']
 
 def print_units_list():
     print("Mass unit converter")
-    print("It supports:", "\nMass: ", list(SI_mass.keys()), "\nLenght: ", list(SI_len.keys()))
+    print("It supports:", "\nMass: ", list(SI_mass.keys()), "\nLenght: ", list(SI_len.keys()), "\nTemperature: ", SI_temp)
 
 def get_value(prompt):
     while True:
@@ -12,7 +13,6 @@ def get_value(prompt):
         if value_str.isdigit():
             return float(value_str)
         print("Invalid input. Input must be number.")
-
 
 def get_unit(prompt):
     while True:
@@ -26,7 +26,24 @@ def convert(value, unit_in, unit_out):
         result = value * SI_mass[unit_in]/SI_mass[unit_out]
     elif unit_in in SI_len:
         result = value * SI_len[unit_in]/SI_len[unit_out]
+    elif unit_in in SI_temp:
+        if unit_in == "C":
+            if unit_out == "K":
+                result = value + 273.15
+            elif unit_out == "F":
+                result = value * 9/5 + 32
+        elif unit_in == "K":
+            if unit_out == "C":
+                result = value - 273.15
+            elif unit_out == "F":
+                result = (value - 273.15) * 9/5 + 32
+        elif unit_in == "F":
+            if unit_out == "C":
+                result = (value - 32) * 5/9
+            elif unit_out == "K":
+                result = (value - 32) * 5/9 + 273.15
     return result
+
 
 def get_result():
     # User input
